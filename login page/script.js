16ce01cae21751efefa10eba9cc796303fb43a6a
arshekhar735 <suman.nwd03@gmail.com>
2024-10-21 23:30:08 +0530

    Add files via upload

---
+++ login page/script.js
@@ -0,0 +1,8 @@
+const registerBtn = document.getElementById('registerBtn');
+const loginBtn = document.getElementById('loginBtn');
+
+// Form submission
+document.getElementById('registerForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for registering with Travel Guide!');
+});
